4178f33f1d905f17394ed48b9f9235fa0953292b
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-12-11 18:22:58 +0100

    _

---
+++ Julekalender/1.des.cs
@@ -0,0 +1,38 @@
+
+namespace Julekalender
+{
+	class Dec1
+	{
+		private static void Log(string msg, params object[] arg)
+		{
+			Console.WriteLine(string.Format(msg, arg));
+		}
+
+		private static void Log(object msg)
+		{
+			Console.WriteLine(msg);
+		}
+		
+		public static void run()
+		{
+			int a;
+
+			using (StreamReader io = new StreamReader("input/input-vekksort.txt"))
+			{
+				List<int> buffer = new List<int>();
+				buffer.Add(int.Parse(io.ReadLine()));
+				int i = buffer[0];
+				while (!io.EndOfStream) {
+					int l = int.Parse(io.ReadLine());
+					if (l >= i)
+					{
+						i = l;
+						buffer.Add(l);
+					}
+				}
+				a = buffer.Sum();
+			}
+			Log(a);
+		}
+	}
+}

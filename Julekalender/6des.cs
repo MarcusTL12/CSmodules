a9adc362c4fd1c69ad104bc2c9fe91ed7a8bad46
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-12-14 22:32:53 +0100

    Started 6.des

---
+++ Julekalender/6des.cs
@@ -0,0 +1,18 @@
+using static Util.CU;
+
+
+namespace Julekalender
+{
+	class Des6
+	{
+		private static int AmtZero(int n)
+		{
+			
+		}
+		
+		public static void Run()
+		{
+			Log("Hei");
+		}
+	}
+}

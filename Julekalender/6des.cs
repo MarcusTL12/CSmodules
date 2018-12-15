769672920b29f1e401a6190c2e6e4756a8736c04
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-12-15 01:41:03 +0100

    6.des done

--- Julekalender/6des.cs
+++ Julekalender/6des.cs
@@ -1,18 +1,22 @@
 using static Util.CU;
+using System.Linq;
 
 
 namespace Julekalender
 {
 	class Des6
 	{
-		private static int AmtZero(int n)
+		private static bool ZeroHeavy(int n)
 		{
-			
+			string s = n.ToString();
+			return s.Aggregate(0, (tot, next) => tot + (next == '0' ? 1 : 0)) > (s.Length / 2);
 		}
 		
 		public static void Run()
 		{
-			Log("Hei");
+			long sum = 0;
+			for (int i = 1; i <= 18163106; i++) sum += (ZeroHeavy(i) ? i : 0);
+			Log(sum);
 		}
 	}
 }
